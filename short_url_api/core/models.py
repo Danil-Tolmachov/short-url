623c245b9125fac1e6070cd9060cdc0db6950623
Danil <pacman11223345@gmail.com>
2022-12-08 21:00:07 +0200

    added link model

--- short_url_api/core/models.py
+++ short_url_api/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.db.models import TextField, DateTimeField
 
-# Create your models here.
+
+class Link(models.Model):
+    source_link = TextField()
+    created_data = DateTimeField()
+
+    def __str__(self):
+        link = str(self.source_link).split('://')[1]
+        host = link.split('/')[0]
+        return host
