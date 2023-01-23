f79596d61c41f51465448ce8e21720d992214206
Danil <59608495+Danil-Tolmachov@users.noreply.github.com>
2023-01-23 18:40:31 +0200

    Update services.py

--- short_url_api/core/services.py
+++ short_url_api/core/services.py
@@ -2,10 +2,19 @@ from core.models import Link
 from core.utils import encode_link, validate_link, decode_link
 
 
-# receives source link
-# returns short link
 def set_link(request, link: str) -> str:
-
+    """
+    Parameters
+    ----------
+    link : str   
+        source link for shortening
+        
+    Returns
+    -------
+    short_link
+        shortened link
+    """
+    
     if not validate_link(link):
         raise ValueError
 
@@ -17,9 +26,20 @@ def set_link(request, link: str) -> str:
     return short_link
 
 
-# receives cyphered(base64) pk
-# returns redirect link
 def get_link(encoded_pk: str) -> str:
+    """
+    Parameters
+    ----------
+    encoded_pk : str   
+        encoded pk (base64)
+        
+    Returns
+    -------
+    source
+        source link
+    """
+    
     decoded_pk = decode_link(encoded_pk)
     source = Link.objects.get(pk=decoded_pk)
+    
     return source
